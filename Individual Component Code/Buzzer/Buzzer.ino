9508226801212ea2dc14e7a2daa82f0cdc811554
ATOIsus <sabinmaharjan884@gmail.com>
2022-03-30 18:45:51 +0545

    Code for Buzzer done.

--- Individual Component Code/Buzzer/Buzzer.ino
+++ Individual Component Code/Buzzer/Buzzer.ino
@@ -1,8 +1,15 @@
+const int buzzerOut = 8; // const not necessary.
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(buzzerOut, OUTPUT);
+
 }
 
 void loop() {
 
+  tone(buzzerOut, 100);
+  delay(1000);
+
+  noTone(buzzerOut);
+  delay(1000);
 }
