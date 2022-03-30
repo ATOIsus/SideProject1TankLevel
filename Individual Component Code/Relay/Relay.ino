dfb71fc4149d507cac9f35ddb090c296a5219804
ATOIsus <sabinmaharjan884@gmail.com>
2022-03-30 19:37:09 +0545

    Code for Relay done.

--- Individual Component Code/Relay/Relay.ino
+++ Individual Component Code/Relay/Relay.ino
@@ -1,8 +1,15 @@
+const int relayOut = 7;  // const not necessary.
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(7, OUTPUT);   // Connected to S terminal of Relay & Digital Pin 7 of Arduino.
 }
 
 void loop() {
+
+  digitalWrite(7, HIGH); // Turn relay ON
+  delay(3000);// Keep it ON for 3 seconds
+
+  digitalWrite(7, LOW);// Turn relay OFF
+  delay(5000);// Keep it OFF for 5 seconds
 
 }
