1b3a5f373bb84b257f6e6a62a1c8a6fbc584f4c6
ATOIsus <sabinmaharjan884@gmail.com>
2022-05-13 18:26:36 +0545

    Basic code completed fully..

--- ard/ard.ino
+++ ard/ard.ino
@@ -1,7 +1,9 @@
 unsigned long firstTime = 0;
 unsigned long secondTime = 0;
+unsigned long lastFillTime = 0;
 int waterState = LOW;
 int stopState = LOW;
+int prevStopState = LOW;
 
 const int waterButton = 2;
 const int stopButton = 3;
@@ -19,24 +21,41 @@ void setup()
 
 void loop()
 {
-  waterState = digitalRead(waterButton);
 
-  if (firstTime + secondTime == 0UL) {
-    if (debounceWater(waterButton) == LOW) {
-      Serial.println("Switch is On for First Time.");
-      firstTime = millis();  // Record the time when switch is turned on or button is pressed.
-    } 
-  }
-
-  if (firstTime > 10) {
+  if (firstTime > 100UL) {
     secondTime = millis(); // Record the secondTime after firstTime is initialized i.e. switch on.
   }
 
-  if (secondTime - firstTime > 60000UL) {
-    Serial.println("A minute has passed.");
+  bool conditionOne = millis() - lastFillTime > 1200000UL && lastFillTime != 0UL;
 
-    if (debounceWater(waterState) == LOW) {
+  if (conditionOne || lastFillTime == 0UL) { // For other time.
+
+    if (firstTime + secondTime == 0UL) {
+      waterState = digitalRead(waterButton);
+
+      if (debounceWater(waterState) == HIGH) {
+        Serial.println("Switch is On for First Time.");
+        firstTime = millis();  // Record the time when switch is turned on or button is pressed.
+      }
+    } 
+
+    if (conditionOne) {
+      // 20 minutes has passed.
+      lastFillTime = 1UL; // Reset the lastFillTime.
+    }
+  }
+
+  bool conditionTwo = firstTime != 0UL && secondTime != 0UL;
+  if (secondTime - firstTime > 30000UL && conditionTwo) {
+
+    Serial.println("30 seconds has passed.");
+
+    waterState = digitalRead(waterButton);
+
+    if (debounceWater(waterState) == HIGH) {
       Serial.println("Switch is On for Second Time.");
+
+      lastFillTime = millis();
 
       tone(buzzer, 100);
       delay(1000);
@@ -46,18 +65,22 @@ void loop()
       delay(1000);
     }
 
-    firstTime = 0;       // Reset the first time.
+    firstTime = 0UL;       // Reset the first time.
   }
 
   stopState = digitalRead(stopButton);
 
-  if (debounceButton(stopState) == LOW) {
+  if (debounceButton(stopState) == HIGH && prevStopState == LOW) {
     Serial.println("Stopping Buzzer");
     noTone(buzzer);
+    prevStopState = HIGH;
   }
 
+  else if (debounceButton(stopState) == LOW && prevStopState == HIGH) {
+    prevStopState = LOW;
+  }
 
-  secondTime = 0;       // Reset the second time.
+  secondTime = 0UL;
 }
 
 
@@ -70,6 +93,8 @@ int debounceWater(int state) {
   }
   return stateNow;
 }
+
+
 
 int debounceButton(int state) {
   int stateNow = digitalRead(stopButton);
